9262b9e8af98aa324b0bc9d28bcdccd3b6dc5dec
hackartist <js.choi.85@gmail.com>
2021-05-09 13:42:09 +0900

    image uploader

---
+++ controller/asset.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const multer = require('multer');
+var router = express.Router();
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images');
+    },
+    filename: (req, file, cb) => {
+        console.log(file);
+        var filetype = file.originalname.substring(file.originalname.length - 3);
+        if (file.mimetype === 'image/gif') {
+            filetype = 'gif';
+        }
+        if (file.mimetype === 'image/png') {
+            filetype = 'png';
+        }
+        if (file.mimetype === 'image/jpeg') {
+            filetype = 'jpg';
+        }
+        cb(null, 'image-' + Date.now() + '.' + filetype);
+    },
+});
+var upload = multer({ storage: storage });
+router.post('/upload', upload.single('file'), function(req, res, next) {
+    console.log(req.file);
+    console.log(req.body);
+    if (!req.file) {
+        res.status(500);
+        return next(err);
+    }
+    res.json({ uri: '/images/' + req.file.filename });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./controller/user');
 const search = require('./controller/search');
+const asset = require('./controller/asset');
 
-mongoose.connect('mongodb://mongo.kas-tutorial:27017/kas-tutorial', { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://mongo.kas-tutorial:27017/kas-tutorial', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use('/v1/user', user);
 app.use('/v1/search', search);
+app.use('/v1/asset', asset);
 
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:3000`);

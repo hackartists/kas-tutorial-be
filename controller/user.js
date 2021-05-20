4107ddcc8afe7964951b82cc795d2071b0161615
hackartist <js.choi.85@gmail.com>
2021-05-20 14:17:49 +0900

    tutorial 1

--- controller/user.js
+++ controller/user.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const User = require('../model/user');
 const wallet = require('../service/kas/wallet');
-const node = require('../service/kas/node');
-const th = require('../service/kas/th');
-const caver = require('caver-js');
 const conv = require('../utils/conv');
 var router = express.Router();
 
-// middleware that is specific to this router
-// router.use(function timeLog(req, res, next) {
-//     console.log('Time: ', Date.now());
-//     next();
-// });
-
 router.post('/', async (req, res) => {
-    const address = await conv.userToAddress(req.body.username);
-    if (address !== '') {
-        res.json({ address });
-        return;
-    }
     // TODO: create an account API
     const account = await wallet.createAccount();
     console.log(account);
+
     // TODO: save address, userid, password
     const user = new User({
         name: req.body.username,
@@ -37,65 +24,6 @@ router.post('/', async (req, res) => {
 
     res.json({
         address: account.address,
-    });
-});
-
-router.get('/:user/klay', async (req, res) => {
-    const address = await conv.userToAddress(req.params.user);
-    // TODO: get balance API
-    const balance = await node.getBalance(address);
-    res.json({
-        balance,
-    });
-});
-
-router.get('/:user/klay/transfer-history', async (req, res) => {
-    const address = await conv.userToAddress(req.params.user);
-    const starttime = req.query['start-timestamp'];
-    const endtime = req.query['end-timestamp'];
-
-    const history = await th.klayHistory(address, starttime, endtime);
-    const ret = [];
-    for (const el of history) {
-        const klay = caver.utils.convertFromPeb(
-            caver.utils.hexToNumberString(el.value),
-            'KLAY',
-        );
-
-        const item = {
-            value: klay,
-            timestamp: el.timestamp,
-        };
-        let target = '';
-
-        if (caver.utils.toChecksumAddress(el.from) === address) {
-            item.eventType = 'sent';
-            target = el.to;
-        } else {
-            item.eventType = 'received';
-            target = el.from;
-        }
-
-        const targetuser = await conv.addressToUser(target);
-
-        item.target = targetuser !== '' ? targetuser : target;
-        ret.push(item);
-    }
-
-    res.json(ret);
-});
-
-router.post('/:user/klay', async (req, res) => {
-    const from = await conv.userToAddress(req.params.user);
-    const to = await conv.userToAddress(req.body.to);
-    const amount = req.body.amount;
-    console.log(from, to, amount);
-
-    // TODO: send KLAY API
-    const txHash = await wallet.sendTrasfer(from, to, amount);
-
-    res.json({
-        txHash,
     });
 });
 
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const user = require('./controller/user');
-const search = require('./controller/search');
-const asset = require('./controller/asset');
-const safe = require('./controller/safe');
 
 mongoose.connect('mongodb://mongo.kas-tutorial:27017/kas-tutorial', {
     useUnifiedTopology: true,
@@ -17,10 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/v1/user', user);
-app.use('/v1/search', search);
-app.use('/v1/asset', asset);
-app.use('/v1/safe', safe);
-
 app.listen(3000, () => {
     console.log(`Example app listening at http://localhost:3000`);
 });
